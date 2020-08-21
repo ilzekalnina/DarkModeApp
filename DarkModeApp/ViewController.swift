a2eb58a62c6b15fc978c64e612a3f02cac420ee7
Ilze Kalnina <ilze@skate.lv>
2020-08-21 16:27:45 +0300

    Wrote code for darkModeTapped

--- DarkModeApp/ViewController.swift
+++ DarkModeApp/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var darkModeButton: UIButton!
+    
+    var isOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("view did load")
+       // darkModeButton.layer.cornerRadius = 10
+
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func darkModeTapped(_ sender: Any) {
+        if isOn {
+            print("Dark mode on")
+            self.isOn = false
+            self.darkModeButton.setTitle("Dark Mode On", for: .normal)
+            self.darkModeButton.setTitleColor(UIColor.white, for: .normal)
+            self.view.backgroundColor = UIColor.black
+        }else{
+            print("Dark mode off")
+            self.isOn = true
+            self.darkModeButton.setTitle("Dark Mode Off", for: .normal)
+            self.darkModeButton.setTitleColor(UIColor.black, for: .normal)
+            self.view.backgroundColor = UIColor.white
+        }
+    }
 }
 
